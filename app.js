50580da2f14702dd54880b2f3eef15c300457544
Inggar Saputra <inggards@gmail.com>
2020-04-16 10:43:39 +0700

    add dotenv & env

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
 const http = require('http').Server(express)
 const socketIo = require('socket.io')(http)
+const port = process.env.port
 
+http.listen(port, () => {
+    console.log('Listening on port', port); 
+})
