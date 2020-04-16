1421f37c7ccbe9bbbb86ccc17279fb25c8cb49d7
Inggar Saputra <inggards@gmail.com>
2020-04-16 11:20:27 +0700

    add control for moving player

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ const http = require('http').Server(express)
 const socketIo = require('socket.io')(http)
 const port = process.env.port
 
+let position = {
+    x: 0
+}
+
+socketIo.on('connection', socket => {
+    socket.emit('position', position);
+    socket.on('move', data => {
+        switch (data) {
+            case "right":
+                position.x += 10
+                socketIo.emit('position', position)
+                break;
+            default:
+                break;
+        }
+    })
+})
+
 http.listen(port, () => {
     console.log('Listening on port', port); 
 })
