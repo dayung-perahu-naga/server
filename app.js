918903c00ff6b64cd573448d120df02170ca327f
Inggar Saputra <inggards@gmail.com>
2020-04-16 11:39:17 +0700

    edit controller and position

--- app.js
+++ app.js
@@ -1,28 +1,78 @@
 require('dotenv').config()
+const express = require ('express') ;
+const app = express() ;
+const http = require('http').createServer(app) ;
+const io = require('socket.io')(http) ;
+const PORT = process.env.PORT || 3000;
 
-const express = require('express')
-const http = require('http').Server(express)
-const socketIo = require('socket.io')(http)
-const port = process.env.port
+let positions= [
+    {
+      right: 80,
+      x: 100,
+      y: 500,
+    },
+    {
+      right: 80,
+      x: 100,
+      y: 620
+    }
+  ]
 
-let position = {
-    x: 0
-}
+let firstId;
+let secondId;
 
-socketIo.on('connection', socket => {
-    socket.emit('position', position);
+io.on('connection', socket => {    
+    socket.emit('positions', positions);
     socket.on('move', data => {
-        switch (data) {
-            case "right":
-                position.x += 10
-                socketIo.emit('position', position)
-                break;
-            default:
-                break;
+
+        if(!firstId && !secondId) {
+            firstId = data.id
+            positions[0].right += 5; // right +5
+            if (positions[0].right <=0 ) {
+                positions[0].right = 0
+                io.emit('positions', positions)
+                io.emit('winner', data.name)
+            } else {
+                io.emit('positions', positions)
+            }
+            
+        } else if(firstId && !secondId && firstId !== data.id) {
+            positions[1].right += 5; // right -5
+                if (positions[1].right <=0 ) {
+                    positions[1].right = 0
+                    io.emit('positions', positions)
+                    io.emit('winner', data.name)
+                } else {
+                    io.emit('positions', positions)
+                }
+
+        } else if(firstId && secondId) {
+
         }
+            if (firstId === data.id) {
+                positions[0].right += 5; // right -5
+                if (positions[0].right <=0 ) {
+                    positions[0].right = 0
+                    io.emit('positions', positions)
+                    io.emit('winner', data.name)
+                } else {
+                    io.emit('positions', positions)
+                }
+            } else if(secondId === data.id){
+                positions[1].right += 5; // right -5
+                if (positions[1].right <=0 ) {
+                    positions[1].right = 0
+                    io.emit('positions', positions)
+                    io.emit('winner', data.name)
+                } else {
+                    io.emit('positions', positions)
+                }
+            }
+
     })
+
 })
 
-http.listen(port, () => {
-    console.log('Listening on port', port); 
+http.listen(PORT, () => {
+    console.log(`listening on PORT: ${PORT}`);
 })
